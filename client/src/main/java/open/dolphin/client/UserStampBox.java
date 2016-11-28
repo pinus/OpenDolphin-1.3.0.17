baa7c8b22f3e453eeb3465e35057f0c9a3f94e4d
pinus <pinus@usagi2>
2016-11-28 15:53:21 +0900

    send stamps from stampbox to KartePane by means of popup menu

--- client/src/main/java/open/dolphin/client/UserStampBox.java
+++ client/src/main/java/open/dolphin/client/UserStampBox.java
@@ -5,13 +5,13 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JPopupMenu;
+import javax.swing.JTextPane;
 import open.dolphin.infomodel.IInfoModel;
 import open.dolphin.infomodel.ModuleInfoBean;
 import open.dolphin.util.StampTreeUtils;
@@ -65,12 +65,8 @@ public class UserStampBox extends AbstractStampBox {
         StampTreeTransferHandler transferHandler = new StampTreeTransferHandler();
 
         // userTrees を順番に並べ替える
-        Collections.sort(userTrees, new Comparator<StampTree>(){
-            @Override
-            public int compare(StampTree o1, StampTree o2) {
-                return tabMap.get(o1.getTreeName()) - tabMap.get(o2.getTreeName());
-            }
-        });
+        Collections.sort(userTrees,
+                (o1, o2) -> tabMap.get(o1.getTreeName()) - tabMap.get(o2.getTreeName()));
 
         // StampBox(TabbedPane) へリスト順に格納する
         // 一つのtabへ一つのtreeが対応
@@ -89,10 +85,7 @@ public class UserStampBox extends AbstractStampBox {
                 orcaIndex = index;
             }
 
-            final StampTreePopupMenu popup = new StampTreePopupMenu(stampTree);
             stampTree.addMouseListener(new MouseAdapter(){
-                private final StampTree thisTree = stampTree;
-
                 @Override
                 public void mousePressed(MouseEvent e) { showPopup(e);}
 
@@ -102,62 +95,29 @@ public class UserStampBox extends AbstractStampBox {
                 private void showPopup(MouseEvent e) {
                     if (e.isPopupTrigger()) {
 
-                        // 傷病名の popup に，カルテに病名を挿入する特別メニューを加える
-                        if (IInfoModel.ENTITY_DIAGNOSIS.equals(thisTree.getTreeInfo().getEntity())) {
-                            StampTreePopupMenu diagPop = new StampTreePopupMenu(thisTree);
+                        StampTreePopupMenu popup = new StampTreePopupMenu(stampTree);
+
+                        if (IInfoModel.ENTITY_DIAGNOSIS.equals(stampTree.getTreeInfo().getEntity())) {
+                            // 傷病名の popup に，カルテに病名を挿入する特別メニューを加える
                             List<ChartImpl> allCharts = ChartImpl.getAllChart();
-
                             if (! allCharts.isEmpty()) {
-                                diagPop.insert(new JPopupMenu.Separator(), 0);
-
-                                for (ChartImpl chart : allCharts) {
-                                    String id = chart.getKarte().getPatient().getPatientId();
-                                    String name = chart.getKarte().getPatient().getFullName();
-                                    final String title = String.format("%s : %s", name, id);
-                                    final DiagnosisDocument doc = chart.getDiagnosisDocument();
-
-                                    // DiagnosisDocument に病名を送るアクション
-                                    Action a = new AbstractAction() {
-                                        private static final long serialVersionUID = 1L;
-                                        {   putValue(Action.NAME, title);
-                                            putValue(Action.SMALL_ICON, GUIConst.ICON_ARROW_UP_LEFT_16);
-                                        }
-                                        @Override
-                                        public void actionPerformed(ActionEvent ev) {
-                                            StampTreeNode selected = thisTree.getSelectedNode();
-                                            if (selected == null) { return; }
-
-                                            List<ModuleInfoBean> stampList = new ArrayList<>();
-
-                                            if (selected.isLeaf()) {
-                                                stampList.add(selected.getStampInfo());
-
-                                            } else {
-                                                // ノードの葉を列挙する
-                                                Enumeration e = selected.preorderEnumeration();
-                                                while (e.hasMoreElements()) {
-                                                    StampTreeNode node = (StampTreeNode) e.nextElement();
-                                                    if (node.isLeaf()) {
-                                                        ModuleInfoBean stampInfo = node.getStampInfo();
-                                                        stampList.add(stampInfo);
-                                                    }
-                                                }
-                                            }
-
-                                            doc.importStampList(stampList, 0);
-                                        }
-                                    };
-                                    // 特別メニューは頭に挿入
-                                    diagPop.insert(a,0);
-                                }
+                                popup.insert(new JPopupMenu.Separator(), 0);
+                                allCharts.forEach(chart -> {
+                                    popup.insert(new SendDiagnosisAction(chart, stampTree), 0);
+                                });
                             }
-                            // 傷病名の popup
-                            diagPop.show(stampTree, e.getX(), e.getY());
 
                         } else {
-                            // それ以外の popup
-                            popup.show(stampTree, e.getX(), e.getY());
+                            // それ以外の popup でも PPane にスタンプを挿入できるようにする
+                            List<Chart> allFrames = EditorFrame.getAllEditorFrames();
+                            if (! allFrames.isEmpty()) {
+                                popup.insert(new JPopupMenu.Separator(), 0);
+                                allFrames.forEach(chart -> {
+                                    popup.insert(new SendStampAction(chart, stampTree), 0);
+                                });
+                            }
                         }
+                        popup.show(stampTree, e.getX(), e.getY());
                     }
                 }
             });
@@ -189,5 +149,98 @@ public class UserStampBox extends AbstractStampBox {
     @Override
     public String getInfo() {
         return BOX_INFO;
+    }
+
+    /**
+     * AbstractAction which sends Stamps to a Chart
+     */
+    private abstract class SendStampBase extends AbstractAction {
+        public final Chart chart;
+        public final StampTree tree;
+
+        public SendStampBase(Chart chart, StampTree tree) {
+            this.chart = chart;
+            this.tree = tree;
+            putValue(Action.NAME, getName(chart));
+            putValue(Action.SMALL_ICON, GUIConst.ICON_ARROW_UP_LEFT_16);
+        }
+        /**
+         * アクションの名前（メニューに表示される文字列）
+         * @param chart
+         * @return
+         */
+        public String getName(Chart chart) {
+            String id = chart.getKarte().getPatient().getPatientId();
+            String patient = chart.getKarte().getPatient().getFullName();
+            return String.format("%s : %s", patient, id);
+        }
+        /**
+         * 選択されたスタンプをリストにする
+         * @return
+         */
+        public List<ModuleInfoBean> getStampList(StampTreeNode selected) {
+
+            List<ModuleInfoBean> stampList = new ArrayList<>();
+
+            if (selected.isLeaf()) {
+                stampList.add(selected.getStampInfo());
+
+            } else {
+                // ノードの葉を列挙する
+                Enumeration e = selected.preorderEnumeration();
+                while (e.hasMoreElements()) {
+                    StampTreeNode node = (StampTreeNode) e.nextElement();
+                    if (node.isLeaf()) {
+                        ModuleInfoBean stampInfo = node.getStampInfo();
+                        stampList.add(stampInfo);
+                    }
+                }
+            }
+            return stampList;
+        }
+    }
+
+    /**
+     * DiagnosisDocument に StampTree から Diagnosis を送る Action
+     */
+    private class SendDiagnosisAction extends SendStampBase {
+        private final DiagnosisDocument doc;
+
+        public SendDiagnosisAction(Chart chart, StampTree tree) {
+            super(chart, tree);
+            doc = ((ChartImpl)chart).getDiagnosisDocument();
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent ev) {
+            StampTreeNode selected = tree.getSelectedNode();
+            if (selected == null) { return; }
+            List<ModuleInfoBean> stampList = getStampList(selected);
+            doc.importStampList(stampList, 0);
+        }
+    }
+
+    /**
+     * Action which sends selected Stamps to PPane
+     */
+    private class SendStampAction extends SendStampBase {
+        private final KartePane pane;
+
+        public SendStampAction(Chart chart, StampTree tree) {
+            super(chart, tree);
+            pane = ((EditorFrame) chart).getEditor().getPPane();
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent ev) {
+            StampTreeNode selected = tree.getSelectedNode();
+            if (selected == null) { return; }
+            List<ModuleInfoBean> stampList = getStampList(selected);
+            // caret を最後に送ってから import する
+            JTextPane textPane = pane.getTextPane();
+            KarteStyledDocument doc = pane.getDocument();
+            textPane.setCaretPosition(doc.getLength());
+            pane.stampInfoDropped(stampList);
+        }
     }
 }
