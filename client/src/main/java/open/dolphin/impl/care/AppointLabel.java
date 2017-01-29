cb764937350390f4d13c4132ccb34ba975367a9c
pinus <pinus@columen>
2017-01-29 19:54:38 +0900

    AppointLabel with VisualRepresentation

--- client/src/main/java/open/dolphin/impl/care/AppointLabel.java
+++ client/src/main/java/open/dolphin/impl/care/AppointLabel.java
@@ -1,55 +1,88 @@
 
 package open.dolphin.impl.care;
 
-import java.awt.Cursor;
-import open.dolphin.infomodel.AppointmentModel;
-import java.awt.dnd.*;
 import java.awt.datatransfer.*;
+import java.awt.event.InputEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.Icon;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
+import open.dolphin.infomodel.AppointmentModel;
+import open.dolphin.ui.PatchedTransferHandler;
 
 /**
  * AppointLabel.
- * draggable label
- * @author  Kauzshi Minagawa, Digital Globe, Inc.
+ * Draggable label.
+ * @author Kauzshi Minagawa, Digital Globe, Inc.
+ * @author pns
  */
-public class AppointLabel extends JLabel implements DragGestureListener,DragSourceListener {
+public class AppointLabel extends JLabel {
     private static final long serialVersionUID = 2843710174202998473L;
-
-    private final DragSource dragSource;
 
     public AppointLabel(String text, Icon icon, int align) {
         super(text, icon, align);
-
-        dragSource = new DragSource();
-        dragSource.createDefaultDragGestureRecognizer(this, DnDConstants.ACTION_COPY_OR_MOVE, this);
+        init();
     }
 
-    @Override
-    public void dragGestureRecognized(DragGestureEvent event) {
+    private void init() {
+        AppointTransferHandler th = new AppointTransferHandler();
+        setTransferHandler(th);
 
-        AppointmentModel appo = new AppointmentModel();
-        appo.setName(this.getText());
-        Transferable t = new AppointEntryTransferable(appo);
-        Cursor cursor = DragSource.DefaultCopyDrop;
-
-        // Starts the drag
-        dragSource.startDrag(event, cursor, t, this);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                th.exportAsDrag(AppointLabel.this, e, AppointTransferHandler.COPY_OR_MOVE);
+            }
+        });
     }
 
-    @Override
-    public void dragDropEnd(DragSourceDropEvent event) {
-    }
-    @Override
-    public void dragEnter(DragSourceDragEvent event) {
-    }
-    @Override
-    public void dragOver(DragSourceDragEvent event) {
-    }
-    @Override
-    public void dragExit(DragSourceEvent event) {
-    }
-    @Override
-    public void dropActionChanged ( DragSourceDragEvent event) {
+    private class AppointTransferHandler extends PatchedTransferHandler {
+        private static final long serialVersionUID = 1L;
+
+        private Icon draggedComp;
+
+        public AppointTransferHandler() {
+            super();
+        }
+
+        @Override
+        protected Transferable createTransferable(JComponent c) {
+            JLabel label = (JLabel)c;
+            AppointmentModel appo = new AppointmentModel();
+            appo.setName(label.getText());
+            return new AppointEntryTransferable(appo);
+        }
+
+        @Override
+        public int getSourceActions(JComponent c) {
+            return COPY_OR_MOVE;
+        }
+
+        /**
+         * 半透明 drag のために dragged component とマウス位置を保存する
+         * @param comp
+         * @param e
+         * @param action
+         */
+        @Override
+        public void exportAsDrag(JComponent comp, InputEvent e, int action) {
+            JLabel label = (JLabel) comp;
+
+            draggedComp = label.getIcon();
+            mousePosition = label.getMousePosition();
+
+            super.exportAsDrag(comp, e, action);
+        }
+
+        /**
+         * フィードバックを返す
+         * @param t
+         * @return
+         */
+        @Override
+        public Icon getVisualRepresentation(Transferable t) {
+            return draggedComp;
+        }
     }
 }
