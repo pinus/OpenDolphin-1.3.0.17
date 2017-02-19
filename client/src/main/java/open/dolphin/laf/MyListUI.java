79e5d0365e717bc0d414da1bf2bc70f9d15ac432
pinus <pinus@columen>
2017-02-19 09:57:59 +0900

    feedback from windows version

--- client/src/main/java/open/dolphin/laf/MyListUI.java
+++ client/src/main/java/open/dolphin/laf/MyListUI.java
@@ -4,13 +4,12 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import javax.swing.JComponent;
-import javax.swing.JViewport;
 import javax.swing.plaf.ComponentUI;
 import javax.swing.plaf.basic.BasicListUI;
 import open.dolphin.client.ClientContext;
 
 /**
- *
+ * ストライプな ListUI.
  * @author pns
  */
 public class MyListUI extends BasicListUI {
@@ -18,49 +17,85 @@ public class MyListUI extends BasicListUI {
     private static final Color DEFAULT_ODD_COLOR = ClientContext.getColor("color.odd");
     private static final Color DEFAULT_EVEN_COLOR = ClientContext.getColor("color.even");
     private static final Color[] ROW_COLORS = {DEFAULT_EVEN_COLOR, DEFAULT_ODD_COLOR};
-
-    private int lastCellHeight = 16;
+    private static final int DEFAULT_ROW_HEIGHT = 16;
 
     public static ComponentUI createUI(JComponent list) {
         return new MyListUI();
     }
-    
-    /**
-     * ストライプな ListUI
-     * @author pns
-     */
+
+    @Override
+    public void installUI(JComponent c) {
+        super.installUI(c);
+    }
+
     @Override
     public void paint(Graphics g, JComponent c) {
+        Object property = list.getClientProperty("Quaqua.List.style");
+        boolean isStriped = property != null && property.equals("striped");
 
-        if ( c.getParent() instanceof JViewport) {
+        if (isStriped) {
+            Rectangle clip = g.getClipBounds();
+            int[] top = getTopY(clip.y);
+            int topY = top[0];
+            int currentRow = top[1]-1;
 
-            // get the row index at the top of the clip bounds (the first row to paint).
-            int rowAtPoint = list.locationToIndex(g.getClipBounds().getLocation());
-            // get the y coordinate of the first row to paint. if there are no rows in the table, start
-            // painting at the top of the supplied clipping bounds.
-            int topY = rowAtPoint < 0 ? g.getClipBounds().y : list.getCellBounds(rowAtPoint, rowAtPoint).y;
+            // ClipBounds.y から topY まで塗る
+            g.setColor(ROW_COLORS[currentRow & 1]);
+            g.fillRect(clip.x, clip.y, clip.width, topY);
+            currentRow ++;
 
-            // create a counter variable to hold the current row. if there are no rows in the table,
-            // start the counter at 0.
-            int currentRow = rowAtPoint < 0 ? 0 : rowAtPoint;
-
-            while (topY < g.getClipBounds().y + g.getClipBounds().height) {
-                Rectangle r = list.getCellBounds(currentRow, currentRow);
-                // if currentRow is out of range, r == null
-                // if FixedCellHeight is not set, getFixedHeight returns -1
-                if (r != null) {
-                    lastCellHeight = r.height;
-                } else {
-                    int h = list.getFixedCellHeight();
-                    if (h > 0) lastCellHeight = h;
-                }
-                int bottomY = topY + lastCellHeight;
+            // 続きを塗る
+            while (topY < clip.y + clip.height) {
+                int bottomY = topY + getCellHeight(currentRow);
                 g.setColor(ROW_COLORS[currentRow & 1]);
-                g.fillRect(g.getClipBounds().x, topY, g.getClipBounds().width, bottomY);
+                g.fillRect(clip.x, topY, clip.width, bottomY);
                 topY = bottomY;
                 currentRow++;
             }
         }
         super.paint(g, c);
+    }
+
+    /**
+     * ClipBound.y を越えた初めての Cell の Y 座標とその行数.
+     * @param clipY
+     * @return
+     */
+    private int[] getTopY(int clipY) {
+        int listSize = list.getModel().getSize();
+
+        if (listSize != 0) {
+            int row = 0;
+            int y = list.getCellBounds(row, row).y;
+            while (y < clipY) {
+                y += getCellHeight(row);
+                row++;
+            }
+            return new int[] { y, row };
+
+        } else  {
+            return new int[] { 0, 0 };
+        }
+    }
+
+    /**
+     * row の高さを得る.
+     * @param row
+     * @return
+     */
+    private int getCellHeight(int row) {
+        int fixed = list.getFixedCellHeight();
+        if (fixed > 0) { return fixed; }
+
+        int listSize = list.getModel().getSize();
+        if (listSize == 0) {
+            // 空のリスト
+            return DEFAULT_ROW_HEIGHT;
+
+        } else {
+            return (row < listSize-1)?
+                    list.getCellBounds(row, row).height:
+                    list.getCellBounds(listSize-1, listSize-1).height;
+        }
     }
 }
