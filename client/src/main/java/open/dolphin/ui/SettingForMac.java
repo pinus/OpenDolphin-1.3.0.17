54d47cc67c1bd0cb593f585fbb67fb713fdbcc1a
pinus <pinus@usagi2>
2017-03-02 12:34:36 +0900

    remove quaqua

--- client/src/main/java/open/dolphin/ui/SettingForMac.java
+++ client/src/main/java/open/dolphin/ui/SettingForMac.java
@@ -2,7 +2,9 @@ package open.dolphin.ui;
 
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
+import javax.swing.UIDefaults;
 import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import open.dolphin.client.Dolphin;
 import open.dolphin.laf.MyButtonUI;
 import open.dolphin.laf.MyComboBoxUI;
@@ -21,6 +23,21 @@ public class SettingForMac {
     private SettingForMac() {}
 
     public static void set(final Dolphin context) {
+        // apple settings
+        System.setProperty("apple.laf.useScreenMenuBar","true"); // ClientContextStub でも設定しているが，そこだと遅いようだ
+        // com.apple.eawt.Application の設定
+        setMacApplication(context);
+
+        // Look and Feel のセットアップ
+        try {
+            // true だと白で書かれてしまう
+            //UIManager.put("Tree.rendererFillBackground", false);
+            UIManager.setLookAndFeel(new MyLookAndFeel());
+
+        } catch (UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace(System.err);
+        }
+
         // JavaFX settings
         // Mac OS X needs this to avoid HeadlessException
         System.setProperty("java.awt.headless", "false");
@@ -34,31 +51,36 @@ public class SettingForMac {
 
         Platform.runLater(() -> {
             ClassLoader jfx = Thread.currentThread().getContextClassLoader();
+            System.out.println("ClassLoader in JFX Thread = " + jfx);
             if (jfx == null) {
                 System.out.println("ClassLoader in JFX Thread is null; set " + contextClassLoader + " instead.");
                 Thread.currentThread().setContextClassLoader(contextClassLoader);
             }
         });
 
-        // apple settings
-        System.setProperty("apple.laf.useScreenMenuBar","true"); // ClientContextStub でも設定しているが，そこだと遅いようだ
-        // Look and Feel のセットアップ
-        setLaf(context);
-        // com.apple.eawt.Application の設定
-        setMacApplication(context);
-
         TouchpadTest.startListening();
     }
 
-    public static void setLaf(Dolphin context) {
-        UIManager.put("TextFieldUI", MyTextFieldUI.class.getName());
-        UIManager.put("PasswordFieldUI", MyPasswordFieldUI.class.getName());
-        UIManager.put("TableUI", MyTableUI.class.getName());
-        UIManager.put("ListUI", MyListUI.class.getName());
-        UIManager.put("TreeUI", MyTreeUI.class.getName());
-        UIManager.put("ButtonUI", MyButtonUI.class.getName());
-        UIManager.put("ToggleButtonUI", MyToggleButtonUI.class.getName());
-        UIManager.put("ComboBoxUI", MyComboBoxUI.class.getName());
+    private static class MyLookAndFeel extends com.apple.laf.AquaLookAndFeel {
+        private static final long serialVersionUID = 1L;
+
+        @Override
+        public UIDefaults getDefaults() {
+            UIDefaults uiDefaults = super.getDefaults();
+
+            uiDefaults.putDefaults(new Object[] {
+                "TextFieldUI", MyTextFieldUI.class.getName(),
+                "PasswordFieldUI", MyPasswordFieldUI.class.getName(),
+                "TableUI", MyTableUI.class.getName(),
+                "ListUI", MyListUI.class.getName(),
+                "TreeUI", MyTreeUI.class.getName(),
+                "ButtonUI", MyButtonUI.class.getName(),
+                "ToggleButtonUI", MyToggleButtonUI.class.getName(),
+                "ComboBoxUI", MyComboBoxUI.class.getName(),
+            });
+
+            return uiDefaults;
+        }
     }
 
     private static void setMacApplication(final Dolphin context) {
