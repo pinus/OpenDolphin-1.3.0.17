d0566b4bab4b1cfc6a57f63eff617d00bfa4fee8
pinus <pinus@usagi2>
2017-09-22 16:38:11 +0900

    test

---
+++ client/src/main/java/open/dolphin/ui/sheet/TestWindow.java
@@ -0,0 +1,29 @@
+package open.dolphin.ui.sheet;
+
+import javax.swing.JButton;
+import javax.swing.JWindow;
+
+/**
+ *
+ * @author pns
+ */
+public class TestWindow extends JWindow {
+    private static final long serialVersionUID = 1L;
+
+
+  
+
+    public static void main (String[] argv) {
+        TestWindow w = new TestWindow();
+        JButton b = new JButton("OK");
+        b.addActionListener(e -> {
+            w.setVisible(false);
+            System.exit(0);
+        });
+        w.add(b);
+        w.setBounds(200, 200, 200, 200);
+        w.setVisible(true);
+
+        System.out.println("window shown");
+    }
+}
