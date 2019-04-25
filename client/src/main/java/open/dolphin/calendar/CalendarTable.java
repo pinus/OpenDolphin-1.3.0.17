16eee49f51075ddbefc4fa6eae5c65bac9f12b81
pinus <pinus@usagi2>
2019-04-25 09:17:36 +0900

    la premi?re ann?e = gan'nen

--- client/src/main/java/open/dolphin/calendar/CalendarTable.java
+++ client/src/main/java/open/dolphin/calendar/CalendarTable.java
@@ -318,7 +318,7 @@ public class CalendarTable extends JTable {
         int year = Integer.valueOf(split[0].substring(1));
         String nengo = Gengo.gengoAlphabetToHalfKanji(nengoAlphabet);
 
-        return String.format("%s%2d", nengo, year);
+        return year == 1 ? String.format("%s元", nengo) : String.format("%s%d", nengo, year);
     }
 
     /**
