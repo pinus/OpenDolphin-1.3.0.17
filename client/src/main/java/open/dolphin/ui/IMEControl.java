7b3547ae3a5f27ce9dee3099bee7194904f5f7ad
pinus@usagi2 <pinus@usagi2>
2020-06-04 16:28:59 +0900

    revisit IMEControl

--- client/src/main/java/open/dolphin/ui/IMEControl.java
+++ client/src/main/java/open/dolphin/ui/IMEControl.java
@@ -1,5 +1,8 @@
 package open.dolphin.ui;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -16,6 +19,7 @@ import java.awt.*;
  * @author pns
  */
 public class IMEControl {
+    private static Logger logger = LoggerFactory.getLogger(IMEControl.class);
 
     static {
         // 「かな」キーで ime on
@@ -105,19 +109,41 @@ public class IMEControl {
 
         JFrame f = new JFrame();
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JTextField tf = new JTextField(100);
-        IMEControl.setImeOnIfFocused(tf);
-        //IMEControl.setImeOffIfFocused(tf);
-        f.add(tf);
+
+        JLabel l1 = new JLabel("TF1");
+        JLabel l2 = new JLabel("TF2");
+        JLabel l3 = new JLabel("LABEL");
+        JTextField tf1 = new JTextField(30);
+        JTextField tf2 = new JTextField(30);
+
+        JButton b1 = new JButton("Get Focus");
+        b1.addActionListener(e -> {
+            SwingUtilities.invokeLater(tf1::requestFocusInWindow);
+        });
+        JButton b2 = new JButton("Get Focus");
+        b2.addActionListener(e -> {
+            SwingUtilities.invokeLater(tf2::requestFocusInWindow);
+        });
+
+        JPanel p1 = new JPanel();
+        p1.setLayout(new BoxLayout(p1, BoxLayout.X_AXIS));
+        p1.add(l1);
+        p1.add(tf1);
+        p1.add(b1);
+        JPanel p2 = new JPanel();
+        p2.setLayout(new BoxLayout(p2, BoxLayout.X_AXIS));
+        p2.add(l2);
+        p2.add(tf2);
+        p2.add(b2);
+        JPanel p3 = new JPanel();
+        p3.add(l3);
+
+        f.getRootPane().setLayout(new BoxLayout(f.getRootPane(), BoxLayout.Y_AXIS));
+        f.getRootPane().add(p1);
+        f.getRootPane().add(p2);
+        f.getRootPane().add(p3);
         f.pack();
+        f.setLocation(200,100);
         f.setVisible(true);
-
-        JFrame f2 = new JFrame();
-        JTextField tf2 = new JTextField(50);
-        IMEControl.setImeOffIfFocused(tf2);
-        f2.add(tf2);
-        f2.pack();
-        f2.setVisible(true);
-
     }
 }
