b1c3a35fa6668cd2c323c21e7df3362b4ebc3e85
pinus@ater <pinus@argentea>
2020-06-21 20:50:22 +0900

    undo example

--- client/src/main/java/Test51.java
+++ client/src/main/java/Test51.java
@@ -1,51 +1,174 @@
-import open.dolphin.client.ClientContext;
-import open.dolphin.client.ClientContextStub;
-import open.dolphin.client.ColorChooserComp;
-import open.dolphin.ui.PNSToggleButton;
-import sun.awt.AppContext;
-
 import javax.swing.*;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.BadLocationException;
 import javax.swing.text.StyledEditorKit;
+import javax.swing.undo.AbstractUndoableEdit;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.CompoundEdit;
+import javax.swing.undo.UndoableEdit;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.geom.AffineTransform;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 /**
- * @author pns
+ * http://java-sl.com/tip_merge_undo_edits.html
  */
-public class Test51 {
+public class Test51 extends JEditorPane {
+    JButton btnUndo=new JButton("Undo");
+    JButton btnRedo=new JButton("Redo");
+    UndoManager undoManager=new UndoManager();
 
-    private void start() {
-        JOptionPane optionPane = new JOptionPane();
-        JDialog dialog = optionPane.createDialog("dialog");
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Merge undoable actions in one group");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final Test51 app = new Test51();
+        JScrollPane scroll = new JScrollPane(app);
+        frame.getContentPane().add(scroll);
 
-        showComp(dialog.getContentPane());
+        JToolBar tb=new JToolBar();
+        app.btnUndo.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                app.undoManager.undo();
+            }
+        });
+        tb.add(app.btnUndo);
+        app.btnRedo.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                app.undoManager.redo();
+            }
+        });
+        tb.add(app.btnRedo);
+        frame.getContentPane().add(tb, BorderLayout.NORTH);
 
-        JWindow w = new JWindow();
-        w.setBackground(new Color(0,0,0,0));
-        w.getRootPane().setOpaque(false);
-        w.add(dialog.getContentPane(), BorderLayout.CENTER);
-        w.pack();
-        w.setLocation(1000,500);
-        w.setVisible(true);
+        frame.setSize(400, 200);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 
-    private void showComp(Component component) {
-        p(component);
-        if (component instanceof JComponent) {
-            for (Component c : ((JComponent)component).getComponents()) {
-                p("parent: " + component);
-                ((JComponent)component).setOpaque(false);
-                showComp(c);
+    public Test51() {
+        super();
+        setEditorKit(new StyledEditorKit());
+        getDocument().addUndoableEditListener(undoManager);
+
+        undoManager.refreshControls();
+    }
+
+    class MyCompoundEdit extends CompoundEdit {
+        boolean isUnDone=false;
+        public int getLength() {
+            return edits.size();
+        }
+
+        public void undo() throws CannotUndoException {
+            super.undo();
+            isUnDone=true;
+        }
+        public void redo() throws CannotUndoException {
+            super.redo();
+            isUnDone=false;
+        }
+        public boolean canUndo() {
+            return edits.size()>0 && !isUnDone;
+        }
+
+        public boolean canRedo() {
+            return edits.size()>0 && isUnDone;
+        }
+
+    }
+    class UndoManager extends AbstractUndoableEdit implements UndoableEditListener {
+        String lastEditName=null;
+        ArrayList<MyCompoundEdit> edits=new ArrayList<MyCompoundEdit>();
+        MyCompoundEdit current;
+        int pointer=-1;
+
+        public void undoableEditHappened(UndoableEditEvent e) {
+            UndoableEdit edit=e.getEdit();
+            if (edit instanceof AbstractDocument.DefaultDocumentEvent) {
+                try {
+                    AbstractDocument.DefaultDocumentEvent event=(AbstractDocument.DefaultDocumentEvent)edit;
+                    int start=event.getOffset();
+                    int len=event.getLength();
+                    String text=event.getDocument().getText(start, len);
+                    boolean isNeedStart=false;
+                    if (current==null) {
+                        isNeedStart=true;
+                    }
+                    else if (text.contains("\n")) {
+                        isNeedStart=true;
+                    }
+                    else if (lastEditName==null || !lastEditName.equals(edit.getPresentationName())) {
+                        isNeedStart=true;
+                    }
+
+                    while (pointer<edits.size()-1) {
+                        edits.remove(edits.size()-1);
+                        isNeedStart=true;
+                    }
+                    if (isNeedStart) {
+                        createCompoundEdit();
+                    }
+
+                    current.addEdit(edit);
+                    lastEditName=edit.getPresentationName();
+
+                    refreshControls();
+                } catch (BadLocationException e1) {
+                    e1.printStackTrace();
+                }
             }
         }
-    }
 
-    private void p(Object o) {
-        System.out.println(o);
-    }
+        public void createCompoundEdit() {
+            if (current==null) {
+                current= new MyCompoundEdit();
+            }
+            else if (current.getLength()>0) {
+                current= new MyCompoundEdit();
+            }
 
-    public static void main(String[] argv) {
-        new Test51().start();
+            edits.add(current);
+            pointer++;
+        }
+
+        public void undo() throws CannotUndoException {
+            if (!canUndo()) {
+                throw new CannotUndoException();
+            }
+
+            MyCompoundEdit u=edits.get(pointer);
+            u.undo();
+            pointer--;
+
+            refreshControls();
+        }
+
+        public void redo() throws CannotUndoException {
+            if (!canRedo()) {
+                throw new CannotUndoException();
+            }
+
+            pointer++;
+            MyCompoundEdit u=edits.get(pointer);
+            u.redo();
+
+            refreshControls();
+        }
+
+        public boolean canUndo() {
+            return pointer>=0;
+        }
+
+        public boolean canRedo() {
+            return edits.size()>0 && pointer<edits.size()-1;
+        }
+
+        public void refreshControls() {
+            btnUndo.setEnabled(canUndo());
+            btnRedo.setEnabled(canRedo());
+        }
     }
 }
