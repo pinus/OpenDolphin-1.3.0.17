090b40580edf0e76234fd8a4cc5233352645bd6c
pinus <pinus@studium>
2023-01-04 05:09:30 +0900

    jlink and jpackage

--- client/appRuntimeImage/jlink.sh
+++
@@ -1,3 +0,0 @@
-rm -rf ./jre
-jlink --no-header-files --no-man-pages --strip-debug --compress=2 --add-modules java.base,java.desktop,java.datatransfer,java.naming,java.prefs,java.xml,java.management,jdk.unsupported --output ./jre
-
