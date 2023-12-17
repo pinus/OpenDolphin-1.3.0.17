bb02a0cc8c812e66e4d9bd9fc6334574f82fdf18
pinus <pinus@studium>
2023-12-17 17:51:59 +0900

    bug fix

--- client/src/main/java/open/dolphin/impl/psearch/PatientSearchPanel.java
+++ client/src/main/java/open/dolphin/impl/psearch/PatientSearchPanel.java
@@ -99,6 +99,13 @@ public class PatientSearchPanel extends MainComponentPanel {
                     popup.show(e.getComponent(), e.getX(), e.getY());
                 }
             }
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                // windows
+                if (e.isPopupTrigger()) {
+                    popup.show(e.getComponent(), e.getX(), e.getY());
+                }
+            }
         });
 
         MainPanel mainPanel = getMainPanel();
